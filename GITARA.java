274e3f320641adb25652d72dea150d1c37355a14
Riseh4rd <112888563+Riseh4rd@users.noreply.github.com>
2022-09-13 18:16:49 +0300

    Create GITARA.java

---
+++ GITARA.java
@@ -0,0 +1,15 @@
+package HW_3;
+
+public class GITARA implements INSTRUMENT{
+	private int KOLI4ESTVO_STRUN;
+
+	public GITARA(int KOLI4ESTVO_STRUN){
+		this.KOLI4ESTVO_STRUN = KOLI4ESTVO_STRUN;
+	}
+
+
+	@Override
+	public void MUZIKA() {
+		System.out.printf("INSTRUMENT: GITARA, KOLI4ESTVO_STRUN: %d\n", this.KOLI4ESTVO_STRUN);
+	}
+}
